524718e8c7b451d262af2440deb3cedf3ae2a354
Vladimir Rubin <vavakado@duck.com>
2023-09-21 15:53:03 +0300

    major code refactor

--- zimey.py
+++ zimey.py
@@ -2,95 +2,83 @@ import pygame
 import time
 import random
 
+# Initialize pygame
 pygame.init()
+
+# Define colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 blue = (0, 0, 255)
+
+# Set up display
 dis_width = 800
 dis_height = 600
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Змейка')
+
+# Set up clock for framerate control
 clock = pygame.time.Clock()
+
+# Define size of each block in the snake
 snake_block = 10
+
+# Define speed of the snake
 snake_speed = 15
-font_style = pygame.font.SysFont(None, 30)
 
-
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 10, dis_height / 3])
-
-
-
-import time
-import random
-
-pygame.init()
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-blue = (0, 0, 255)
-dis_width = 800
-dis_height = 600
-dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Змейка')
-clock = pygame.time.Clock()
-snake_block = 10
-snake_speed = 15
+# Set up font style for text rendering
 font_style = pygame.font.SysFont(None, 30)
 
 def message(msg, color):
    mesg = font_style.render(msg, True, color)
    dis.blit(mesg, [dis_width/10, dis_height/3])
 
+
 def gameLoop():
-   game_over = False
-   game_close = False
+   is_game_over = False
+   is_game_close = False
    x1 = dis_width / 2
    y1 = dis_height / 2
    x1_change = 0 # Переменная скорости
    y1_change = 0
    foodx = random.randint(20,dis_width-20) # Рандомизация положния яблока
    foody = random.randint(20,dis_height-20)
+   game_over_message = 'Вы проиграли! Нажмите Q для выхода или C для повторной игры'
 
-   while not game_over:
-       while game_close == True:
+   while not is_game_over:
+       while is_game_close:
            dis.fill(white)
-           message("Вы проиграли! Нажмите Q для выхода или C для повторной игры", red)
+           message(game_over_message, red)
            pygame.display.update()
            for event in pygame.event.get():
                if event.type == pygame.KEYDOWN:
                    if event.key == pygame.K_q:
-                       game_over = True
-                       game_close = False
+                       is_game_over = True
+                       is_game_close = False
                    if event.key == pygame.K_c:
                        gameLoop()
                if event.type == pygame.QUIT:
-                   game_over = True
-                   game_close=False
+                   is_game_over = True
+                   is_game_close = False
+       
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
-               game_over = True
+               is_game_over = True
            if event.type == pygame.KEYDOWN:
-               if event.key == pygame.K_a:
-                   x1_change = -snake_block
-                   y1_change = 0
-               elif event.key == pygame.K_d:
-                   x1_change = snake_block
-                   y1_change = 0
-               elif event.key == pygame.K_w:
-                   y1_change = -snake_block
-                   x1_change = 0
-               elif event.key == pygame.K_s:
-                   y1_change = snake_block
-                   x1_change = 0
+               direction = {
+                   pygame.K_a: (-snake_block, 0),  # Move left
+                   pygame.K_d: (snake_block, 0),  # Move right
+                   pygame.K_w: (0, -snake_block),  # Move up
+                   pygame.K_s: (0, snake_block)  # Move down
+               }.get(event.key, (0, 0))
+               x1_change, y1_change = direction
+       
        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-           game_close = True
+           is_game_close = True
        x1 += x1_change
        y1 += y1_change
        dis.fill(white)
-       if foodx-10<x1<foodx+10 and foody-10<y1<foody+10:
+       if foodx - 10 < x1 < foodx + 10 and foody - 10 < y1 < foody + 10: # Check for food consumption
            foodx = random.randint(20, dis_width - 20)
            foody = random.randint(20, dis_height - 20)
 
